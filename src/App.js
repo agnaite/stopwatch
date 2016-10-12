6d19cd5963f444868d879cb35eaeaf536e4b6d61
Agnė Klimaitė <agnaite@me.com>
2016-10-11 20:23:11 -0700

    stopwatch is working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
 
+var timerId = '';
+
 var Stopwatch = React.createClass({
   getInitialState : function() {
     return {seconds : 0, minutes : 0, hours : 0};
   },
   startWatch : function() {
+    timerId = setInterval(this.updateTime, 1000);
     console.log('start');
   },
   stopWatch : function() {
+    clearInterval(timerId);
     console.log('stop');
   },
   clearWatch : function() {
+    clearInterval(timerId);
     console.log('clear');
+  },
+  updateTime : function() {
+    var newSeconds = this.state.seconds;
+    var newMinutes = this.state.minutes;
+    var newHours = this.state.hours;
+
+    newSeconds++;
+        if(newSeconds >= 60){
+          newSeconds = 0;
+          newMinutes++;
+          if(newMinutes >=60){
+            newMinutes = 0;
+            newHours++;
+          }
+        }
+        this.displayNewTime(newHours, newMinutes, newSeconds);
+  },
+  displayNewTime : function(hours, minutes, seconds) {
+    this.setState({hours : hours, minutes : minutes, seconds : seconds});
+
   },
   render : function() {
     return <div>
               <div className="stopwatch">
-                <div id="hours">00</div>:
-                <div id="minutes">00</div>:
-                <div id="seconds">00</div>
+                <div id="hours">{this.state.hours}</div>:
+                <div id="minutes">{this.state.minutes}</div>:
+                <div id="seconds">{this.state.seconds}</div>
               </div>
               <div className="buttons">
                 <button id="start" onClick={this.startWatch}>Start</button>
